1811780a8ddf283e919967ea759e6f5288900cac
TangerinePeelFrank <157260111+TangerinePeelFrank@users.noreply.github.com>
2025-06-24 22:06:21 +0800

    Add files via upload

---
+++ connect_to_eth.py
@@ -0,0 +1,45 @@
+import json
+from web3 import Web3
+from web3.middleware import ExtraDataToPOAMiddleware
+from web3.middleware import geth_poa_middleware
+from web3.providers.rpc import HTTPProvider
+
+'''
+If you use one of the suggested infrastructure providers, the url will be of the form
+now_url  = f"https://eth.nownodes.io/{now_token}"
+alchemy_url = f"https://eth-mainnet.alchemyapi.io/v2/{alchemy_token}"
+infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
+'''
+
+def connect_to_eth():
+	infura_token = "d6a4f0802df242cc9219f9d77b2e4ff8"
+	url = f"https://mainnet.infura.io/v3/{infura_token}"
+	w3 = Web3(HTTPProvider(url))
+	assert w3.is_connected(), f"Failed to connect to provider at {url}"
+	return w3
+
+
+def connect_with_middleware(contract_json):
+	with open(contract_json, "r") as f:
+		d = json.load(f)
+		d = d['bsc']
+		address = d['address']
+		abi = d['abi']
+
+	infura_token = "d6a4f0802df242cc9219f9d77b2e4ff8"
+	bnb_testnet_url = "	https://bsc-testnet.infura.io/v3/{infura_token}"
+	w3 = Web3(HTTPProvider(bnb_testnet_url))
+	assert w3.is_connected(), f"Failed to connect to BNB testnet at {bnb_testnet_url}"
+
+
+	# The second section requires you to inject middleware into your w3 object and
+	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
+	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
+	w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+	contract = w3.eth.contract(address=address, abi=abi)
+
+	return w3, contract
+
+
+if __name__ == "__main__":
+	connect_to_eth()
