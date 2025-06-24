bfb5c4b9ee47ec2926df0a94cbb99c77be5fd01a
TangerinePeelFrank <157260111+TangerinePeelFrank@users.noreply.github.com>
2025-06-24 22:37:13 +0800

    Add files via upload

--- ipfs.py
+++ ipfs.py
@@ -5,14 +5,14 @@ def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	#YOUR CODE HERE
 	json_data = json.dumps(data)
-	post = requests.post("http://127.0.0.1:5001/api/v0/add", files={"file": json_data})
+	post = requests.post("https://ipfs.infura.io:5001/api/v0", files={"file": json_data})
 	if post.status_code == 200:
 		cid = post.json().get("Hash")
 		return cid
 
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
-	response = requests.post("http://127.0.0.1:5001/api/v0/cat", params={"arg": cid})
+	response = requests.post("https://ipfs.infura.io:5001/api/v0/cat", params={"arg": cid})
 	if response.status_code == 200:
 		if content_type == "json":
 			data = json.loads(response.text)
