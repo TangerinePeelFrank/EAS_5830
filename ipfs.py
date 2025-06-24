bc9fc137ad90d61da26cdd71ff4cdaefca26bb98
TangerinePeelFrank <157260111+TangerinePeelFrank@users.noreply.github.com>
2025-06-24 22:56:04 +0800

    Add files via upload

--- ipfs.py
+++ ipfs.py
@@ -1,25 +1,20 @@
 import requests
 import json
 
-pinata_pin_url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
-pinata_gateway_url = "https://gateway.pinata.cloud/ipfs/"
-api_key = "7328a830365b6d32db56"
-api_secret = "4258b90fd40ee48465190b9afb793baf6a2818953fa235110d93859de0f44048"
-
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	#YOUR CODE HERE
 	json_data = json.dumps(data)
     
 	headers = {
-        'pinata_api_key': api_key,
-        'pinata_secret_api_key': api_secret
+        'pinata_api_key': "7328a830365b6d32db56",
+        'pinata_secret_api_key': "4258b90fd40ee48465190b9afb793baf6a2818953fa235110d93859de0f44048"
     }
     
 	files = {
         'file': ('data.json', json_data)
     }
-	response = requests.post(pinata_pin_url, files=files, headers=headers)
+	response = requests.post("https://api.pinata.cloud/pinning/pinFileToIPFS", files=files, headers=headers)
     
 	if response.status_code == 200:
 		cid = response.json()["IpfsHash"]
@@ -29,7 +24,7 @@ def pin_to_ipfs(data):
 
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
-	url = f"{pinata_gateway_url}{cid}"
+	url = f"{"https://gateway.pinata.cloud/ipfs/"}{cid}"
     
 	response = requests.get(url)
     
