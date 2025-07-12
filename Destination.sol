75590670b333b528dc87d03ee1ff482f75196751
TangerinePeelFrank <157260111+TangerinePeelFrank@users.noreply.github.com>
2025-07-12 15:08:05 +0800

    Add files via upload

--- Destination.sol
+++ Destination.sol
@@ -25,29 +25,29 @@ contract Destination is AccessControl {
 	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
 		require(underlying_tokens[_underlying_token] != address(0), "Token not registered");
         address wrappedTokenAddress = underlying_tokens[_underlying_token];
-        require(wrappedTokenAddress != address(0), "Wrapped token address not found");
-        BridgeToken bridgeToken = BridgeToken(wrappedTokenAddress);
-        bridgeToken.mint(_recipient, _amount);
-        emit Wrap(_underlying_token, wrappedTokenAddress, _recipient, _amount);
+		require(wrappedTokenAddress != address(0), "Wrapped token address not found");
+		BridgeToken bridgeToken = BridgeToken(wrappedTokenAddress);
+		bridgeToken.mint(_recipient, _amount);
+		emit Wrap(_underlying_token, wrappedTokenAddress, _recipient, _amount);
 	}
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
 		BridgeToken bridgeToken = BridgeToken(_wrapped_token);
-        require(bridgeToken.balanceOf(msg.sender) >= _amount, "Insufficient balance");
-        bridgeToken.burnFrom(msg.sender, _amount);
-        address underlyingTokenAddress = wrapped_tokens[_wrapped_token];
-        require(underlyingTokenAddress != address(0), "Underlying token address not found");
-        emit Unwrap(underlyingTokenAddress, _wrapped_token, msg.sender, _recipient, _amount);
+		require(bridgeToken.balanceOf(msg.sender) >= _amount, "Insufficient balance");
+		bridgeToken.burnFrom(msg.sender, _amount);
+		address underlyingTokenAddress = wrapped_tokens[_wrapped_token];
+		require(underlyingTokenAddress != address(0), "Underlying token address not found");
+		emit Unwrap(underlyingTokenAddress, _wrapped_token, msg.sender, _recipient, _amount);
 	}
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
 		require(underlying_tokens[_underlying_token] == address(0), "Token already exists");
-        BridgeToken bridgeToken = new BridgeToken(_underlying_token, name, symbol, address(this));
-        underlying_tokens[_underlying_token] = address(bridgeToken);
-        wrapped_tokens[address(bridgeToken)] = _underlying_token;
-        tokens.push(address(bridgeToken));
-        emit Creation(_underlying_token, address(bridgeToken));
-        return address(bridgeToken);
+		BridgeToken bridgeToken = new BridgeToken(_underlying_token, name, symbol, address(this));
+		underlying_tokens[_underlying_token] = address(bridgeToken);
+		wrapped_tokens[address(bridgeToken)] = _underlying_token;
+		tokens.push(address(bridgeToken));
+		emit Creation(_underlying_token, address(bridgeToken));
+		return address(bridgeToken);
 	}
 
 }
