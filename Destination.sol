e0bd632931a4a32f33e86ed4b06d12f187cd4919
TangerinePeelFrank <157260111+TangerinePeelFrank@users.noreply.github.com>
2025-07-12 15:16:22 +0800

    Add files via upload

--- Destination.sol
+++ Destination.sol
@@ -25,32 +25,24 @@ contract Destination is AccessControl {
 	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
 		address wrappedTokenAddress = wrapped_tokens[_underlying_token];
         require(wrappedTokenAddress != address(0), "Token not registered");
-
         BridgeToken(wrappedTokenAddress).mint(_recipient, _amount);
-
         emit Wrap(_underlying_token, wrappedTokenAddress, _recipient, _amount);
 	}
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
 		address underlyingTokenAddress = underlying_tokens[_wrapped_token];
         require(underlyingTokenAddress != address(0), "Token not registered");
-
         require(BridgeToken(_wrapped_token).balanceOf(msg.sender) >= _amount, "Insufficient balance");
-
         BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
-
         emit Unwrap(underlyingTokenAddress, _wrapped_token, msg.sender, _recipient, _amount);
 	}
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
 		BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, address(this));
         address wrappedAddress = address(newToken);
-
         wrapped_tokens[_underlying_token] = wrappedAddress;
         underlying_tokens[wrappedAddress] = _underlying_token;
-
         tokens.push(_underlying_token);
-
         emit Creation(_underlying_token, wrappedAddress);
         return wrappedAddress;
 	}
